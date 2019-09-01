92780789cc2b7c6110dd68bf227e3b42834f5e8d
DxExWxExY <dxexwxexy@gmail.com>
2019-08-31 22:32:11 -0600

    Added Main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import sys
 from PyQt5.QtWidgets import QApplication
-
 from views.main_window import MainWindow
 
 if __name__ == "__main__":
     #Add your classes executions here
     app = QApplication([])
     mainWindow = MainWindow()
-    app.exec_()
+    sys.exit(app.exec_())
