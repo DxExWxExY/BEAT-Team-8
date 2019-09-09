6aa6e4b86fe735761af6ff228f90f2c4e1b0e8f5
DxExWxExY <DxExWxExY@gmail.com>
2019-09-09 16:33:31 -0600

    Finished Project Tab

--- views/tabs/project_tab.py
+++ views/tabs/project_tab.py
@@ -4,7 +4,6 @@ from common.tab_layout import TabLayout
 
 class ProjectTab(TabLayout):
     def __init__(self):
-        # your constructor must make the following calls
         super().__init__("Project View", "Detailed Project View")
         super().addContetentToLeftPanel(self.leftPanelBuilder())
         super().addContetentToRightPanel(self.rightPanelBuilder())
@@ -32,9 +31,9 @@ class ProjectTab(TabLayout):
         projctName = QLineEdit()
         projctDescription = QTextEdit()
         binPath = QLineEdit()
-        binProperties = QTextEdit()
         browsePath = QPushButton('Browse')
-        # spacer = QSpacerItem(100, 100, )
+        deleteButton = QPushButton('Delete')
+        saveButton = QPushButton('Save')
 
         layout.addWidget(QLabel("Project Name"), 0, 0)
         layout.addWidget(QLabel('Project Description'), 1, 0)
@@ -44,10 +43,11 @@ class ProjectTab(TabLayout):
         layout.addWidget(projctName, 0, 1)
         layout.addWidget(projctDescription, 1, 1, 2, 1)
         layout.addWidget(binPath, 3, 1)
-        layout.addWidget(binProperties, 4, 1, 2, 1)
+        layout.addWidget(self.tableBuilder(), 4, 1)
 
+        layout.addWidget(deleteButton, 5, 0)
+        layout.addWidget(saveButton, 5, 2)
         layout.addWidget(browsePath, 3, 2)
-        # layout.addWidget(spacer)
 
         layout.setContentsMargins(100, 0, 100, 0)
 
@@ -63,6 +63,31 @@ class ProjectTab(TabLayout):
         searchButton = QPushButton('Search')
 
         layout.addWidget(searchBox, 0, 0, 1, 4)
-        layout.addWidget(searchButton, 0, 4 , 1, 2)
+        layout.addWidget(searchButton, 0, 4, 1, 2)
 
         return layout
+
+    def tableBuilder(self):
+        table = QTableWidget()
+        table.setRowCount(13)
+        table.setColumnCount(2)
+
+        table.setItem(0, 0, QTableWidgetItem('OS'))
+        table.setItem(1, 0, QTableWidgetItem('Binary Type'))
+        table.setItem(2, 0, QTableWidgetItem('Machine'))
+        table.setItem(3, 0, QTableWidgetItem('Class'))
+        table.setItem(4, 0, QTableWidgetItem('Bits'))
+        table.setItem(5, 0, QTableWidgetItem('Language'))
+        table.setItem(6, 0, QTableWidgetItem('Canary'))
+        table.setItem(7, 0, QTableWidgetItem('Crypto'))
+        table.setItem(8, 0, QTableWidgetItem('Nx'))
+        table.setItem(9, 0, QTableWidgetItem('Pic'))
+        table.setItem(10, 0, QTableWidgetItem('Relocs'))
+        table.setItem(11, 0, QTableWidgetItem('Relro'))
+        table.setItem(12, 0, QTableWidgetItem('Stripped'))
+
+        table.setHorizontalHeaderLabels(['Name', 'Value'])
+        table.verticalHeader().setVisible(False)
+        table.horizontalHeader().setStretchLastSection(True)
+
+        return table
