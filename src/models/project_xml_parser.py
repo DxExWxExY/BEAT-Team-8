3649b4f82eccf44212b661d8879fda0247b24471
DxExWxExY <DxExWxExY@gmail.com>
2019-09-23 22:23:14 -0600

    WIP

--- src/models/project_xml_parser.py
+++ src/models/project_xml_parser.py
@@ -7,7 +7,21 @@ name = etree.SubElement(project, "name")
 description = etree.SubElement(project, "description")
 binaryPath = etree.SubElement(project, "binaryPath")
 binaryProperties = etree.SubElement(project, "binaryProperties")
-
+'''
+os = etree.SubElement(binary, "")
+binaryType = etree.SubElement(binary, "")
+machine = etree.SubElement(binary, "")
+binaryClass = etree.SubElement(binary, "")
+bits = etree.SubElement(binary, "")
+language = etree.SubElement(binary, "")
+canary = etree.SubElement(binary, "")
+crypto = etree.SubElement(binary, "")
+nx = etree.SubElement(binary, "")
+pic = etree.SubElement(binary, "")
+relocs = etree.SubElement(binary, "")
+relro = etree.SubElement(binary, "")
+stripped = etree.SubElement(binary, "")
+'''
 name.text = "Project 1"
 
 print(etree.tostring(project, pretty_print=True))
