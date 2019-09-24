0bbb9bbc36d99f61fcab100e245bebc682303c6a
DxExWxExY <DxExWxExY@gmail.com>
2019-09-23 22:20:28 -0600

    WIP

--- src/models/project_xml_parser.py
+++ src/models/project_xml_parser.py
@@ -1,5 +1,16 @@
 from xmlschema import XMLSchema
+from lxml import etree
 schema = XMLSchema("C:\\Users\\DxExWxExY\\PycharmProjects\\BEAT\\res\\project_schema.xsd")
+
+project = etree.Element("project")
+name = etree.SubElement(project, "name")
+description = etree.SubElement(project, "description")
+binaryPath = etree.SubElement(project, "binaryPath")
+binaryProperties = etree.SubElement(project, "binaryProperties")
+
+name.text = "Project 1"
+
+print(etree.tostring(project, pretty_print=True))
 
 # class ProjectSchemaParser:
 #     def __init__(self):
