ac66e3269986c957a2c41e9564a7375527fbbbac
Alan Balderas <alan9716@hotmail.com>
2019-10-22 18:38:51 -0600

    DEV-2 Modified Dynamic Analysis & Added Breakpoints

--- src/models/dynamic_analysis.py
+++ src/models/dynamic_analysis.py
@@ -20,15 +20,34 @@ class DynamicAnalysis:
 
     def __dynamicAnalysis(self, command):
         if self.analyzer is not None:
-            #Continue Program Execution
-            self.analyzer.cmd("dc")
-            # Print Binary Information into the BEAT Prompt
-           return self.analyzer.cmd("i1j")
+            #Execute Dynamic Analysis
+            self.analyzer.cmdj(command)
 
-    def exitAnalysis (self, command):
+    def __Breakpoints(self)
+        # Print Binary Information into the BEAT Prompt
+        binaryInfo = self.__dynamicAnalysis("i1j")
+        #Find references on binary
+        findReferences = self.__dynamicAnalysis("axtj")
+
+         for i in range(len(findReferences)):
+            # Add breakpoint
+            breakPoint = self.__dynamicAnalysis("db")  + hex(findReferences[i]["from"])
+            #Add breakpoint at desired location
+            run = self.__dynamicAnalysis(breakpoint)
+
+        while True:
+            #Run until breakpoint is checked
+            run = self.__dynamicAnalysis("dc")
+            #Execute on breakpoint reference call
+            execute = self.__dynamicAnalysis("dso")
+        if self.analyzer is not None:
+            # Continue Program Execution Until Breakpoint
+            self.__dynamicAnalysis("dc")
+
+    def __exitAnalysis (self):
         # Exit Binary Dynamic Analysis
         #self.analyzer.cmd("exit")
         self.analyzer.quit()
         print("Exited Dynamic Analysis.")
 
-
+    #TODO Modify Dynamic Analysis & Breakpoints
