687750efc9dafc230e2fafa65afe8caae8dd7773
DxExWxExY <DxExWxExY@gmail.com>
2019-10-29 17:24:29 -0600

    Fixed Dynamic Syntax

--- src/analyzers/dynamic_analysis.py
+++ src/analyzers/dynamic_analysis.py
@@ -1,3 +1,5 @@
+import os
+
 import r2pipe
 
 
@@ -6,27 +8,52 @@ class DynamicAnalysis:
 
     def setPath(self, path):
         try:
-            # Open the binary file
             self.analyzer = r2pipe.open(path)
-            # Analyze the binary
             self.analyzer.cmd("aaa")
-            # Enter debbuger mode
             self.analyzer.cmd("doo")
-            # Ready for Binary Analysis
             if self.analyzer.info().stripped:
                 print("The Binary is ready for dynamic analysis")
         except:
             self.analyzer = None
             print("Error could not perform dynamic analysis on the binary file.")
 
-    def __dynamicAnalysis(self, command):
+    def __executej(self, command):
         if self.analyzer is not None:
-            # Continue Program Executation
-            self.analyzer.cmd("dc")
-            # Print Binary Information into the BEAT Prompt
-        return self.analyzer.cmd("i1j")
+            return self.analyzer.cmdj(command)
 
-    def exitAnalysis(self, command):
+    def __execute(self, command):
+        if self.analyzer is not None:
+            self.analyzer.cmd(command)
+
+    def __breakpoints(self):
+        # Print Binary Information into the BEAT Prompt
+        binaryInfo = self.__executej("i1j")
+        # Find references on binary
+        findReferences = self.__executej("axtj")
+
+        for i in range(len(findReferences)):
+            # Add breakpoint
+            breakPoint = self.__executej("db") + hex(findReferences[i]["from"])
+            # Add breakpoint at desired location
+            run = self.__executej(breakpoint)
+
+        while True:
+            # Run until breakpoint is checked
+            run = self.__executej("dc")
+            # Execute on breakpoint reference call
+            execute = self.__executej("dso")
+
+        if self.analyzer is not None:
+            # Continue Program Execution Until Breakpoint
+            self.__executej("dc")
+
+
+    def __exitAnalysis(self):
         # Exit Binary Dynamic Analysis
-        self.analyzer.cmd("exit")
+        # self.analyzer.cmd("exit")
+        self.analyzer.quit()
         print("Exited Dynamic Analysis.")
+
+if __name__ == "__main__":
+    a = DynamicAnalysis()
+    a.setPath('res%sex.o' % os.sep)
