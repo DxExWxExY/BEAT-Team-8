8e25f825b093063eec521cc397d7ad4dfded07d2
DxExWxExY <DxExWxExY@gmail.com>
2019-10-30 12:29:19 -0600

    WIP

--- src/analyzers/dynamic_analysis.py
+++ src/analyzers/dynamic_analysis.py
@@ -1,4 +1,6 @@
-import os
+import base64
+import os, sys
+import time
 
 import r2pipe
 
@@ -11,8 +13,7 @@ class DynamicAnalysis:
             self.analyzer = r2pipe.open(path)
             self.analyzer.cmd("aaa")
             self.analyzer.cmd("doo")
-            if self.analyzer.info().stripped:
-                print("The Binary is ready for dynamic analysis")
+            print("The Binary is ready for dynamic analysis")
         except:
             self.analyzer = None
             print("Error could not perform dynamic analysis on the binary file.")
@@ -25,27 +26,51 @@ class DynamicAnalysis:
         if self.analyzer is not None:
             self.analyzer.cmd(command)
 
-    def __breakpoints(self):
-        # Print Binary Information into the BEAT Prompt
-        binaryInfo = self.__executej("i1j")
-        # Find references on binary
-        findReferences = self.__executej("axtj")
+    def breakpoints(self):
+        bpSet = []
+        self.__execute("e dbg.bpinmaps=0")
+        results = self.__executej("isj")
+        for i in range(len(results)):
+            address = hex(results[i]['vaddr'])
+            if address != hex(0) and address not in bpSet:
+                bpSet.append(address)
+                print(f"db {address}")
+                self.__execute(f"db {address}")
+        # print(self.__executej("dbj"))
+        self.runDynamic()
+        # print(self.__executej("afvrj"))
 
-        for i in range(len(findReferences)):
-            # Add breakpoint
-            breakPoint = self.__executej("db") + hex(findReferences[i]["from"])
-            # Add breakpoint at desired location
-            run = self.__executej(breakpoint)
 
+    def runDynamic(self):
+        self.__execute("ood")
         while True:
-            # Run until breakpoint is checked
-            run = self.__executej("dc")
-            # Execute on breakpoint reference call
-            execute = self.__executej("dso")
+            self.__execute("dc")
+            # self.__execute("s")
+            var = self.__executej("sj")
+            print(hex(var[0]['offset']), end="\n")
+            print(self.__executej(f"axtj {hex(var[0]['offset'])}"))
+            time.sleep(1)
+        pass
 
-        if self.analyzer is not None:
-            # Continue Program Execution Until Breakpoint
-            self.__executej("dc")
+        # # Print Binary Information into the BEAT Prompt
+        # binaryInfo = self.__executej("i1j")
+        # # Find references on binary
+        # findReferences = self.__executej("axtj")
+        #
+        # for i in range(len(findReferences)):
+        #     # Add breakpoint
+        #     breakPoint = self.__executej("db") + hex(findReferences[i]["from"])
+        #     # Add breakpoint at desired location
+        #     run = self.__executej(breakpoint)
+        #
+        # while True:
+        #     # Run until breakpoint is checked
+        #     run = self.__executej("dc")
+        #     # Execute on breakpoint reference call
+        #     execute = self.__executej("dso")
+        #
+        # if self.analyzer is not None:
+        #     self.__executej("dc")
 
 
     def __exitAnalysis(self):
@@ -56,4 +81,6 @@ class DynamicAnalysis:
 
 if __name__ == "__main__":
     a = DynamicAnalysis()
-    a.setPath('res%sex.o' % os.sep)
+    a.setPath('..\\..\\res%sex.o' % os.sep)
+    a.breakpoints()
+
