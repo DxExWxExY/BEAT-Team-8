c566ac6bfdb51bab01e6c6cd4d09842ffc6efe9d
jrdonzello <jrdonzello@miners.utep.edu>
2019-11-10 16:11:23 -0700

    sprint5 -Justen

--- src/storage/output_generator.py
+++ src/storage/output_generator.py
@@ -1,2 +1,20 @@
 import jinja2
 
+class OutputGeneration():
+    global file
+
+    def __init__(self):
+        pass
+
+    # function for user to enter their own lines of code.
+    def createForm(self):
+        pass
+
+    def __createFile(self, name, fileType):
+        fileName = name + fileType
+        self.file = open(fileName, "w")
+
+    # generate strings, structs, variables.
+    def __generateVars(self):
+
+    
