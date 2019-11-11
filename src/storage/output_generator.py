5b47ca61c2790d9f44e78aaca23029433ead5d00
jrdonzello <jrdonzello@miners.utep.edu>
2019-11-10 17:50:24 -0700

    sprint5 -Justen

--- src/storage/output_generator.py
+++ src/storage/output_generator.py
@@ -1,6 +1,6 @@
-import jinja2
-
-class OutputGeneration():
+from jinja2 import Template
+import re
+class OutputGenerator:
     global file
 
     def __init__(self):
@@ -10,11 +10,46 @@ class OutputGeneration():
     def createForm(self):
         pass
 
-    def __createFile(self, name, fileType):
+    def createFile(self, name, fileType):
         fileName = name + fileType
         self.file = open(fileName, "w")
 
-    # generate strings, structs, variables.
-    def __generateVars(self):
+    # write to output file
+    def __writeToFile(self, data):
+        self.file.write(data)
 
-    
+    #Werer assiming that the point of innterest will have its python quivalent as a string tied to it
+    def fillInTemplate(self, type, data):
+        pass
+
+    # enter function name ONLY, dictionary of data(?)
+    # key = flag, value = string
+    # scenarios of function body:
+    #   1. data initialization
+    #   2. return 'value' (?)
+    #   3. other function calls
+    # parameter 'data':
+    #   data[0] = function name
+    #   data[1] = function return value
+    #   data[2] = function parameters
+    def generateFunction(self, data):
+        func_name = data[0]
+        func_ret = data[1]
+        try:
+            func_para = data[2]
+        except IndexError:
+            func_para = ""
+        tm = Template("def {{func_name}}({{func_para}}): \n   {{func_ret}}\n")
+        line = tm.render(func_name=func_name,func_body=func_ret, func_para=func_para)
+        self.__writeToFile(line)
+
+    # generate strings, structs, variables.
+    # must provide type, name, and value of variable to create.
+    def generateVars(self, type, name, value):
+        tm = Template("{{type}} {{name}} = {{value}}\n")
+        line = tm.render(type=type, name=name, value=value)
+        self.__writeToFile(line)
+
+outputGen = OutputGenerator()
+list = ["printMethod","printf(\"hello world\")"]
+outputGen.generateFunction(list)
